357370eac40af06bc80614ea2ccb54a7d58ace7f
mdabrowski25 <m.dabrowski97@o2.pl>
2020-09-23 21:12:19 +0200

    deleted imports from Main.java

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,10 +1,5 @@
 package pl.sdacademy;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.*;
-
 public class Main {
     public static void main(String[] args) {
         ApiDataProvider apiDataProvider = new ApiDataProvider();
