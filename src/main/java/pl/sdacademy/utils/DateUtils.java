bff15a4275b2a5a3f7158173d57eab49ca284376
mdabrowski25 <m.dabrowski97@o2.pl>
2020-09-24 20:46:30 +0200

    D4 v2

--- src/main/java/pl/sdacademy/utils/DateUtils.java
+++ src/main/java/pl/sdacademy/utils/DateUtils.java
@@ -13,7 +13,7 @@ public class DateUtils {
         return DAYS.between(earlierDate, laterDate);
     }
 
-    public boolean isDatesDifferent(Collection<LocalDate> localDates){
+    public boolean isDatesDifferent(Collection<LocalDate> localDates) {
         List<LocalDate> collect = localDates.stream()
                 .sorted()
                 .collect(Collectors.toList());
@@ -27,10 +27,10 @@ public class DateUtils {
         return flag;
     }
 
-public long countFarthestDateDifference(List<LocalDate> localDates){
-    List<LocalDate> collect = localDates.stream()
-            .sorted()
-            .collect(Collectors.toList());
-    return countDays(collect.get(0), collect.get(collect.size() - 1));
-}
+    public long countFarthestDateDifference(List<LocalDate> localDates) {
+        List<LocalDate> collect = localDates.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        return countDays(collect.get(0), collect.get(collect.size() - 1));
+    }
 }
