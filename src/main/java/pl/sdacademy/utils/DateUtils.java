1ed9674fcd75d0c83103ec2cbc97afdbee3bdaa8
mdabrowski25 <m.dabrowski97@o2.pl>
2020-09-24 12:31:07 +0200

    inlined return in method countDays in DateUtils and marked D2 as done

--- src/main/java/pl/sdacademy/utils/DateUtils.java
+++ src/main/java/pl/sdacademy/utils/DateUtils.java
@@ -7,7 +7,6 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public class DateUtils {
 
     public long countDays(LocalDate earlierDate, LocalDate laterDate) {
-        long daySpread = DAYS.between(earlierDate, laterDate);
-        return daySpread;
+        return DAYS.between(earlierDate, laterDate);
     }
 }
