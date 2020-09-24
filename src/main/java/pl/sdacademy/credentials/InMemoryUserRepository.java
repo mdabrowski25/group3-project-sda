5c791c5a6cd691a8e2adbe945f8a683900bc7f7d
Wojtas93 <67224349+Wojtas93@users.noreply.github.com>
2020-09-24 18:31:38 +0200

    C3

--- src/main/java/pl/sdacademy/credentials/InMemoryUserRepository.java
+++ src/main/java/pl/sdacademy/credentials/InMemoryUserRepository.java
@@ -3,23 +3,22 @@ package pl.sdacademy.credentials;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import pl.sdacademy.SessionFactoryProvider;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class InMemoryUserRepository implements UserRepository {
-    private List<User> users = Arrays.asList(
-            new User("Wladek", "Waldkowicz", LocalDate.of(1989, 12, 12), false),
-            new User("Kazek", "Nowak", LocalDate.of(1976, 2, 27), false),
-            new User("Admin", "Adminowicz", LocalDate.of(2000, 1, 1), true));
+    private static List<User> users = new ArrayList<>();
+
+    public static void addExampleUsers() {
+        users.add(new User("Wladek", "Waldkowicz", LocalDate.of(1989, 12, 12), false));
+        users.add(new User("Kazek", "Nowak", LocalDate.of(1976, 2, 27), false));
+        users.add(new User("Admin", "Adminowicz", LocalDate.of(2000, 1, 1), true));
+    }
+
 
     @Override
     public User readById(int id) {
@@ -36,7 +35,7 @@ public class InMemoryUserRepository implements UserRepository {
         SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory("hibernate.cfg.xml");
         Session session = sessionFactory.openSession();
         Query<User> query = session.createQuery("SELECT u FROM User u WHERE u.id > 0", User.class);
-        List <User> users = query.getResultList();
+        List<User> users = query.getResultList();
         session.close();
         sessionFactory.close();
         return users;
