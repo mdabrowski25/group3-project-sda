4ab13ddf2814c1d023b1d7fdff13c544ea524869
Wojtas93 <67224349+Wojtas93@users.noreply.github.com>
2020-09-24 19:01:24 +0200

    Test C4

--- src/main/java/pl/sdacademy/credentials/InMemoryUserRepository.java
+++ src/main/java/pl/sdacademy/credentials/InMemoryUserRepository.java
@@ -8,16 +8,22 @@ import pl.sdacademy.SessionFactoryProvider;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InMemoryUserRepository implements UserRepository {
-    private List<User> users = new ArrayList<>();
+    private List<User> users = Arrays.asList(
+            new User("Wladek", "Waldkowicz", LocalDate.of(1989, 12, 12), false),
+            new User("Kazek", "Nowak", LocalDate.of(1976, 2, 27), false),
+            new User("Admin", "Adminowicz", LocalDate.of(2000, 1, 1), true)
+    );
 
-    public void addExampleUsers() {
-        users.add(new User("Wladek", "Waldkowicz", LocalDate.of(1989, 12, 12), false));
-        users.add(new User("Kazek", "Nowak", LocalDate.of(1976, 2, 27), false));
-        users.add(new User("Admin", "Adminowicz", LocalDate.of(2000, 1, 1), true));
-    }
+
+  // public void addExampleUsers() {
+  //     users.add(new User("Wladek", "Waldkowicz", LocalDate.of(1989, 12, 12), false));
+  //     users.add(new User("Kazek", "Nowak", LocalDate.of(1976, 2, 27), false));
+  //     users.add(new User("Admin", "Adminowicz", LocalDate.of(2000, 1, 1), true));
+  // }
 
     public List<User> getUsers() {
         return users;
