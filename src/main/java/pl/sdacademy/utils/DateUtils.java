770d09516c566f42d3f09371242b7436cc102d4d
mdabrowski25 <m.dabrowski97@o2.pl>
2020-09-24 20:36:15 +0200

    D3 v2

--- src/main/java/pl/sdacademy/utils/DateUtils.java
+++ src/main/java/pl/sdacademy/utils/DateUtils.java
@@ -19,10 +19,13 @@ public class DateUtils {
                 .collect(Collectors.toList());
         boolean flag = true;
         for (int i = 0; i < collect.size(); i++) {
-            if (collect.get(i) == collect.get(i+1)){
+            if (collect.get(i) == collect.get(i + 1)) {
                 flag = false;
+                break;
             }
         }
         return flag;
     }
+
+
 }
