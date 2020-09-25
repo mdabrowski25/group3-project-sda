61ec54c6d51423f31a01bbe9cbfb7469d0fde376
mdabrowski25 <m.dabrowski97@o2.pl>
2020-09-25 15:44:14 +0200

    DA6 rebuild (still on it)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module pl.sdacademy {
     requires org.hibernate.orm.core;
     requires java.persistence;
     requires java.naming;
+    opens pl.sdacademy;
     exports pl.sdacademy.JavaFx;
     exports pl.sdacademy;
 }
