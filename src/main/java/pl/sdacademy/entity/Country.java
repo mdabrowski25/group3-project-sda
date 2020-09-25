52d4ad86a772f9cd03725e3d27bef2018b3b6c52
Wojtas93 <67224349+Wojtas93@users.noreply.github.com>
2020-09-25 15:06:23 +0200

    DB1, DB2

---
+++ src/main/java/pl/sdacademy/entity/Country.java
@@ -0,0 +1,41 @@
+package pl.sdacademy.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String shortname;
+    private int population;
+
+    public Country() {
+    }
+
+    public Country(String name, String shortname, int population) {
+        this.name = name;
+        this.shortname = shortname;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortname() {
+        return shortname;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
---
+++ src/main/java/pl/sdacademy/entity/DayData.java
@@ -0,0 +1,71 @@
+package pl.sdacademy.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Entity
+public class DayData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private LocalDate date;
+    @OneToOne
+    private Country country;
+    private int total_cases;
+    private int total_new_cases_today;
+    private int total_active_cases;
+    private int total_new_deaths_today;
+    private int total_deaths;
+    private int total_recovered;
+
+    public DayData() {
+    }
+
+    public DayData(LocalDate date, Country country, int total_cases,
+                   int total_new_cases_today, int total_active_cases, int total_new_deaths_today,
+                   int total_deaths, int total_recovered) {
+        this.date = date;
+        this.country = country;
+        this.total_cases = total_cases;
+        this.total_new_cases_today = total_new_cases_today;
+        this.total_active_cases = total_active_cases;
+        this.total_new_deaths_today = total_new_deaths_today;
+        this.total_deaths = total_deaths;
+        this.total_recovered = total_recovered;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public int getTotal_cases() {
+        return total_cases;
+    }
+
+    public int getTotal_new_cases_today() {
+        return total_new_cases_today;
+    }
+
+    public int getTotal_active_cases() {
+        return total_active_cases;
+    }
+
+    public int getTotal_new_deaths_today() {
+        return total_new_deaths_today;
+    }
+
+    public int getTotal_deaths() {
+        return total_deaths;
+    }
+
+    public int getTotal_recovered() {
+        return total_recovered;
+    }
+}
