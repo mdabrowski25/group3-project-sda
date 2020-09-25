18bb3e9184110f8faea40f3992f9d7bc1f463d7b
Wojtas93 <67224349+Wojtas93@users.noreply.github.com>
2020-09-25 14:36:03 +0200

    C11 - C15

--- src/main/java/pl/sdacademy/credentials/HibernateUserRepository.java
+++ src/main/java/pl/sdacademy/credentials/HibernateUserRepository.java
@@ -1,30 +1,69 @@
 package pl.sdacademy.credentials;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import pl.sdacademy.SessionFactoryProvider;
+
 import java.util.List;
 
 public class HibernateUserRepository implements UserRepository {
+
     @Override
     public User readById(int id) {
-        return null;
+        SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory("hibernate.cfg.xml");
+        Session session = sessionFactory.openSession();
+        User user = session.get(User.class, id);
+        if (user==null){
+            return null;
+        }
+        session.close();
+        sessionFactory.close();
+        return user;
     }
 
     @Override
     public List<User> readAll() {
-        return null;
+        SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory("hibernate.cfg.xml");
+        Session session = sessionFactory.openSession();
+        Query<User> query = session.createQuery("SELECT u FROM User u", User.class);
+        List<User> users = query.getResultList();
+        session.close();
+        sessionFactory.close();
+        return users;
     }
 
     @Override
     public void create(User user) {
-
+        SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory("hibernate.cfg.xml");
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 
     @Override
     public void update(User user) {
-
+        SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory("hibernate.cfg.xml");
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(user);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 
     @Override
     public void delete(User user) {
-
+        SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory("hibernate.cfg.xml");
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 }
