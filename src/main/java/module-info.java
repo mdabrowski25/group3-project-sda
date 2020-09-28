bac8bffcc644d1be6d39de48bff7bb3e45a79263
mdabrowski25 <m.dabrowski97@o2.pl>
2020-09-28 11:17:02 +0200

    Added some module-path lines due to lack of some 'require' lines

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,12 @@ module pl.sdacademy {
     requires org.hibernate.orm.core;
     requires java.persistence;
     requires java.naming;
+    requires net.bytebuddy;
+    requires com.fasterxml.classmate;
+    requires java.xml.bind;
+    requires com.sun.xml.bind;
+    opens pl.sdacademy.entity;
+    opens pl.sdacademy.credentials;
     opens pl.sdacademy;
     exports pl.sdacademy.JavaFx;
     exports pl.sdacademy;
