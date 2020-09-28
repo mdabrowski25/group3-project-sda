f883c2c517758fccf45700c6033e6dafdc95e6c3
KatarzynaMolenda <67151343+KatarzynaMolenda@users.noreply.github.com>
2020-09-28 19:19:23 +0200

    DB5 poprawione - już bardziej wygląda jak coś o co mogło chodzić ;)

--- src/main/java/pl/sdacademy/entity/JdbcCovidDao.java
+++ src/main/java/pl/sdacademy/entity/JdbcCovidDao.java
@@ -1,17 +1,40 @@
 package pl.sdacademy.entity;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcCovidDao implements CovidDao{
+private Connection connection;
 
+public JdbcCovidDao(Connection connection) {
+    this.connection = connection;
+}
 
     @Override
     public List<Country> getCountries() {
-        List<Country> allCountries = new ArrayList<>();
-        return allCountries;
+                try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("" +
+                    "SELECT 'name, shortname, population'" +
+                    "FROM" +
+                    "country");
+            List<Country> allCountries = new ArrayList<>();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String shortname = resultSet.getString("shortname");
+                int population = resultSet.getInt("population");
+                allCountries.add(new Country(id, name, shortname, population));
+            }
+            return allCountries;
+        } catch (SQLException e) {
+            throw new RuntimeException("Błąd", e);
+        }
     }
 
     @Override
